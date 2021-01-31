552885de08384809c24cae350490baf6f8cda317
Patrice Cottman <patrice.cottman@gmail.com>
2021-01-31 18:37:38 -0500

    Initiliazing app.  Got package.json and webpack and filesystem set up.  Need to make express server.

---
+++ Client/src/index.jsx
@@ -0,0 +1,5 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<p>Hello, from index.jsx!</p>, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/Client/src');
+const PUBLIC_DIR = path.join(__dirname, '/Client/public');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: PUBLIC_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+};
